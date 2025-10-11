e192839cd4bf8f73a183db01ad972dc08b35699d
greguz <gregolettogiacomo@gmail.com>
2025-10-11 17:09:03 +0200

    Some tests

--- src/update/fields.spec.ts
+++ src/update/fields.spec.ts
@@ -1,9 +1,26 @@
 import test from 'ava'
 
-import { wrapBSON } from '../lib/bson.js'
-import { $inc } from './fields.js'
+import { unwrapBSON, wrapBSON } from '../lib/bson.js'
+import type { BSONNode } from '../lib/node.js'
+import { $inc, $rename } from './fields.js'
+
+function bind<T extends BSONNode>(
+  fn: (...args: BSONNode[]) => T,
+  ...right: unknown[]
+): (...left: unknown[]) => unknown {
+  return (...left: unknown[]) =>
+    unwrapBSON(fn(...left.map(wrapBSON), ...right.map(wrapBSON)))
+}
 
 test('$inc', t => {
-  t.is($inc(wrapBSON(null), wrapBSON(-2)).value, -2)
-  t.is($inc(wrapBSON(44), wrapBSON(-2)).value, 42)
+  const inc = bind($inc)
+
+  t.is(inc(null, -2), -2)
+  t.is(inc(44, -2), 42)
+})
+
+test('$rename', t => {
+  const rename = bind($rename)
+
+  t.deepEqual(rename({ _value: 42 }, '_value', 'value'), { value: 42 })
 })
